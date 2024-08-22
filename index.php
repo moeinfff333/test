a7b6fab8a3e2a687eb302da533e0e38a6e5c6908
imMoeinFadakar <moeinfadakar8@gmail.com~>
2024-08-23 00:11:20 +0330

    init project

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?=
+
+$moein = "fadakar";
+$hassan = "mamad";
+
+
+?>
+
+
+
+
+   <script src="app.js" ></script>
+</body>
+
+</html>
